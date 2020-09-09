9ef2fea72d43c56b1961997a83db2e3e7702f8bb
Patrick Heemskerk <pheemskerk@inforit.nl>
2020-09-09 17:16:17 +0200

    Corrected formatting.

--- src/DotNetCore.CAP/Internal/ISubscribeInvoker.Default.cs
+++ src/DotNetCore.CAP/Internal/ISubscribeInvoker.Default.cs
@@ -15,101 +15,101 @@ using Microsoft.Extensions.Logging;
 
 namespace DotNetCore.CAP.Internal
 {
-  public class SubscribeInvoker : ISubscribeInvoker
-  {
-    private readonly ILogger _logger;
-    private readonly IServiceProvider _serviceProvider;
-    private readonly ISerializer _serializer;
-    private readonly ConcurrentDictionary<int, ObjectMethodExecutor> _executors;
-
-    public SubscribeInvoker(ILoggerFactory loggerFactory, IServiceProvider serviceProvider, ISerializer serializer)
+    public class SubscribeInvoker : ISubscribeInvoker
     {
-      _serviceProvider = serviceProvider;
-      _serializer = serializer;
-      _logger = loggerFactory.CreateLogger<SubscribeInvoker>();
-      _executors = new ConcurrentDictionary<int, ObjectMethodExecutor>();
-    }
+        private readonly ILogger _logger;
+        private readonly IServiceProvider _serviceProvider;
+		private readonly ISerializer _serializer;
+        private readonly ConcurrentDictionary<int, ObjectMethodExecutor> _executors;
 
-    public async Task<ConsumerExecutedResult> InvokeAsync(ConsumerContext context, CancellationToken cancellationToken = default)
-    {
-      cancellationToken.ThrowIfCancellationRequested();
-
-      var methodInfo = context.ConsumerDescriptor.MethodInfo;
-
-      _logger.LogDebug("Executing subscriber method : {0}", methodInfo.Name);
-
-      var executor = _executors.GetOrAdd(methodInfo.MetadataToken, x => ObjectMethodExecutor.Create(methodInfo, context.ConsumerDescriptor.ImplTypeInfo));
-
-      using var scope = _serviceProvider.CreateScope();
-
-      var provider = scope.ServiceProvider;
-
-      var obj = GetInstance(provider, context);
-
-      var message = context.DeliverMessage;
-      var parameterDescriptors = context.ConsumerDescriptor.Parameters;
-      var executeParameters = new object[parameterDescriptors.Count];
-      for (var i = 0; i < parameterDescriptors.Count; i++)
-      {
-        if (parameterDescriptors[i].IsFromCap)
+		public SubscribeInvoker(ILoggerFactory loggerFactory, IServiceProvider serviceProvider, ISerializer serializer)
         {
-          executeParameters[i] = new CapHeader(message.Headers);
+            _serviceProvider = serviceProvider;
+			_serializer = serializer;
+            _logger = loggerFactory.CreateLogger<SubscribeInvoker>();
+            _executors = new ConcurrentDictionary<int, ObjectMethodExecutor>();
         }
-        else
+
+        public async Task<ConsumerExecutedResult> InvokeAsync(ConsumerContext context, CancellationToken cancellationToken = default)
         {
-          if (message.Value != null)
-          {
-            if (_serializer.IsJsonType(message.Value))  // use ISerializer when reading from storage, skip other objects if not Json
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var methodInfo = context.ConsumerDescriptor.MethodInfo;
+
+            _logger.LogDebug("Executing subscriber method : {0}", methodInfo.Name);
+
+            var executor = _executors.GetOrAdd(methodInfo.MetadataToken, x => ObjectMethodExecutor.Create(methodInfo, context.ConsumerDescriptor.ImplTypeInfo));
+
+            using var scope = _serviceProvider.CreateScope();
+
+            var provider = scope.ServiceProvider;
+
+            var obj = GetInstance(provider, context);
+
+            var message = context.DeliverMessage;
+            var parameterDescriptors = context.ConsumerDescriptor.Parameters;
+            var executeParameters = new object[parameterDescriptors.Count];
+            for (var i = 0; i < parameterDescriptors.Count; i++)
             {
-              executeParameters[i] = _serializer.Deserialize(message.Value, parameterDescriptors[i].ParameterType);
+                if (parameterDescriptors[i].IsFromCap)
+                {
+                    executeParameters[i] = new CapHeader(message.Headers);
+                }
+                else
+                {
+                    if (message.Value != null)
+                    {
+						if (_serializer.IsJsonType(message.Value))  // use ISerializer when reading from storage, skip other objects if not Json
+                        {
+							executeParameters[i] = _serializer.Deserialize(message.Value, parameterDescriptors[i].ParameterType);
+                        }
+                        else
+                        {
+                            var converter = TypeDescriptor.GetConverter(parameterDescriptors[i].ParameterType);
+                            if (converter.CanConvertFrom(message.Value.GetType()))
+                            {
+                                executeParameters[i] = converter.ConvertFrom(message.Value);
+                            }
+                            else
+                            {
+                                executeParameters[i] = Convert.ChangeType(message.Value, parameterDescriptors[i].ParameterType);
+                            }
+                        }
+                    }
+                }
             }
-            else
-            {
-              var converter = TypeDescriptor.GetConverter(parameterDescriptors[i].ParameterType);
-              if (converter.CanConvertFrom(message.Value.GetType()))
-              {
-                executeParameters[i] = converter.ConvertFrom(message.Value);
-              }
-              else
-              {
-                executeParameters[i] = Convert.ChangeType(message.Value, parameterDescriptors[i].ParameterType);
-              }
-            }
-          }
+
+            var resultObj = await ExecuteWithParameterAsync(executor, obj, executeParameters);
+            return new ConsumerExecutedResult(resultObj, message.GetId(), message.GetCallbackName());
         }
-      }
 
-      var resultObj = await ExecuteWithParameterAsync(executor, obj, executeParameters);
-      return new ConsumerExecutedResult(resultObj, message.GetId(), message.GetCallbackName());
+        protected virtual object GetInstance(IServiceProvider provider, ConsumerContext context)
+        {
+            var srvType = context.ConsumerDescriptor.ServiceTypeInfo?.AsType();
+            var implType = context.ConsumerDescriptor.ImplTypeInfo.AsType();
+
+            object obj = null;
+            if (srvType != null)
+            {
+                obj = provider.GetServices(srvType).FirstOrDefault(o => o.GetType() == implType);
+            }
+
+            if (obj == null)
+            {
+                obj = ActivatorUtilities.GetServiceOrCreateInstance(provider, implType);
+            }
+
+            return obj;
+        }
+
+        private async Task<object> ExecuteWithParameterAsync(ObjectMethodExecutor executor, object @class, object[] parameter)
+        {
+            if (executor.IsMethodAsync)
+            {
+                return await executor.ExecuteAsync(@class, parameter);
+            }
+
+            return executor.Execute(@class, parameter);
+        }
     }
-
-    protected virtual object GetInstance(IServiceProvider provider, ConsumerContext context)
-    {
-      var srvType = context.ConsumerDescriptor.ServiceTypeInfo?.AsType();
-      var implType = context.ConsumerDescriptor.ImplTypeInfo.AsType();
-
-      object obj = null;
-      if (srvType != null)
-      {
-        obj = provider.GetServices(srvType).FirstOrDefault(o => o.GetType() == implType);
-      }
-
-      if (obj == null)
-      {
-        obj = ActivatorUtilities.GetServiceOrCreateInstance(provider, implType);
-      }
-
-      return obj;
-    }
-
-    private async Task<object> ExecuteWithParameterAsync(ObjectMethodExecutor executor, object @class, object[] parameter)
-    {
-      if (executor.IsMethodAsync)
-      {
-        return await executor.ExecuteAsync(@class, parameter);
-      }
-
-      return executor.Execute(@class, parameter);
-    }
-  }
 }
